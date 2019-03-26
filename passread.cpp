239874cbf5c57964f2925f1654c72ad90139e51d
hwrnr <hawerner.hawerner@gmail.com>
2019-03-26 11:10:47 +0100

    Read u+200b as space and remove tabs from file

--- passread.cpp
+++ passread.cpp
@@ -11,51 +11,60 @@ using namespace std;
 
 
 void printUsernameAndPassword(const string& username, const string& password){
-	string command = "echo -n \'";
-	cout << "Username: " << username << endl;
-	cout << "Password: * * *" << endl;
-	command += password;
-	command += "\' | xclip -l 1";
-	system(reinterpret_cast<const char*>(command.c_str()));
+  string command = "echo -n \'";
+  cout << "Username: " << username << endl;
+  cout << "Password: * * *" << endl;
+  command += password;
+  command += "\' | xclip -l 1";
+  system(reinterpret_cast<const char*>(command.c_str()));
 }
 
+bool replace(std::string& str, const std::string& from, const std::string& to) {
+    size_t start_pos = str.find(from);
+    if(start_pos == std::string::npos)
+        return false;
+    str.replace(start_pos, from.length(), to);
+    return true;
+}
 
 int main(int argc, char const *argv[]){
 
-	if(argc != 2){
-		cout << "Error. You need to enter exactly one argument" << endl;
-		return 0;
-	}
+  if(argc != 2){
+    cout << "Error. You need to enter exactly one argument" << endl;
+    return 0;
+  }
 
-	string homePath, filePath, username, password, key, usernameSalt, passwordSalt;
-	homePath = getenv("HOME");
-	filePath = homePath;
-	filePath += PATH;
-	filePath += argv[1];
+  string homePath, filePath, username, password, key, usernameSalt, passwordSalt;
+  homePath = getenv("HOME");
+  filePath = homePath;
+  filePath += PATH;
+  filePath += argv[1];
 
-	cout << "Enter encryption key: ";
+  cout << "Enter encryption key: ";
   system("stty -echo");
-	cin >> key;
+  cin >> key;
   system("stty echo");
   cout << endl;
 
-	ifstream file;
-	file.open(filePath);
+  ifstream file;
+  file.open(filePath);
 
-	file >> usernameSalt;
-	file >> username;
-	file >> passwordSalt;
-	file >> password;
-	username = base64_decode(username);
-	password = base64_decode(password);
-	crypt(username, key, usernameSalt);
-	crypt(password, key, passwordSalt);
+  file >> usernameSalt;
+  file >> username;
+  file >> passwordSalt;
+  file >> password;
+  username = base64_decode(username);
+  password = base64_decode(password);
+  crypt(username, key, usernameSalt);
+  crypt(password, key, passwordSalt);
+
+  replace(username, "\u200b", " ");
+  replace(password, "\u200b", " ");
+
+  printUsernameAndPassword(username, password);
+  file.close();
 
 
-	printUsernameAndPassword(username, password);
-	file.close();
 
-
-
-	return 0;
+  return 0;
 }
